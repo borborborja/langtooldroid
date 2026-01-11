cdb86fbda400bd9e7dcc5948d774e28d235be966
Borja Balsera <borja@micapum.net>
2026-01-12 00:46:28 +0100

    Bump version to 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.borja.langtooldroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.1.7"
+        versionCode = 10
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
